fc4a86eb75385210d0899af5a5417128d30cad1c
RdMateus <mateus.domingues.rodrigues@gmail.com>
2025-11-08 11:42:22 +0000

    Frontend Basic Template

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,12 +12,21 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
-        'css/templatemo-villa-agency.css',
+        'template/vendor/bootstrap/css/bootstrap.min.css',
+        'template/css/fontawesome.css',
+        'template/css/templatemo-villa-agency.css',
+        'template/css/owl.css',
+        'template/css/animate.css',
+        'template/css/site.css',
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
     ];
     public $js = [
-        'js/'
+        'template/vendor/jquery/jquery.min.js',
+        'template/vendor/bootstrap/js/bootstrap.bundle.min.js',
+        'template/js/isotope.min.js',
+        'template/js/owl-carousel.js',
+        'template/js/counter.js',
+        'template/js/custom.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -15,6 +15,7 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -22,59 +23,61 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
 
-<!-- ***** Header Area Start Comprador ***** -->
-  <header class="header-area header-sticky">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
-                    <a href="<?= \yii\helpers\Url::home() ?>" class="nav-logo">
-                        <img src="<?= Yii::getAlias('@web') ?>/img/Find4ULogo" alt="Logo">
-                    </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Menu Start ***** -->
-                    <ul class="nav">
-                      <li><a href="#" class="active">Home</a></li>
-                      <li><a href="#">Imóveis</a></li>
-                        <!--=== Aqui colocar if se é compardor ou anunciante=== -->
-                      <li class="btnVisit"><a href="#" ><i class="fa fa-calendar"></i> Marcar Visita</a></li>
-                      <li class="btnVisit"><a href="#" ><i class="fa fa-chart-line"></i> Meus Anuncios</a></li>
-                      <li class="btnFa"><a href="#"><i class="fa fa-heart"></i></a></li>
-                      <li class="btnFa"><a href="#"><i class="fa fa-user"></i></a></li>
-                    </ul>
-                    <a class='menu-trigger'>
-                        <span>Menu</span>
-                    </a>
-                    <!-- ***** Menu End ***** -->
-                </nav>
+<body class="d-flex flex-column h-100">
+    <?php $this->beginBody() ?>
+
+    <!-- ***** Header Area Start Comprador ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="<?= \yii\helpers\Url::home() ?>" class="nav-logo">
+                            <img src="<?= Yii::getAlias('@web') ?>/img/Find4ULogo" alt="Logo">
+                        </a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li><a href="#" class="active">Home</a></li>
+                            <li><a href="#">Imóveis</a></li>
+                            <!--=== Aqui colocar if se é compardor ou anunciante=== -->
+                            <li class="btnVisit"><a href="#"><i class="fa fa-calendar"></i> Marcar Visita</a></li>
+                            <li class="btnVisit"><a href="#"><i class="fa fa-chart-line"></i> Meus Anuncios</a></li>
+                            <li class="btnFa"><a href="#"><i class="fa fa-heart"></i></a></li>
+                            <li class="btnFa"><a href="#"><i class="fa fa-user"></i></a></li>
+                        </ul>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
             </div>
         </div>
-    </div>
-  </header>
-  <!-- ***** Header Area End Comprador***** -->
+    </header>
+    <!-- ***** Header Area End Comprador***** -->
 
-<main role="main" class="flex-shrink-0">
-    <div class="container">
+    <main role="main" class="flex-shrink-0">
+
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
         <?= $content ?>
-    </div>
-</main>
 
-<footer class="footer mt-auto py-3 text-muted">
-    <div class="container">
-        <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-end"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+    </main>
 
-<?php $this->endBody() ?>
+    <footer class="footer mt-auto py-3 text-muted" style="background-color: black; color: white;">
+        <div class="container">
+            <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+            <p class="float-end"><?= Yii::powered() ?></p>
+        </div>
+    </footer>
+
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage();
