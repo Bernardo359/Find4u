942ca89847698189a9b6d6f28668e5f66e4aaba2
Bernanatex359 <2232337@my.ipleiria.pt>
2025-11-27 15:09:59 +0000

    AboutUs Done

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -6,6 +6,51 @@ use yii\helpers\Html;
 
 $this->title = 'About';
 ?>
+<div class="main-title">
+    <h1>Find4U</h1>
+</div>
+<div class="about-section">
+    <h3 class="about-title">Sobre Nós</h3>
+    <div class="about-text">
+        <p>O Find4U é o seu novo marketplace imobiliário, criado para simplificar a forma como procura, vende ou arrenda imóveis. Aqui encontra uma plataforma intuitiva, moderna e transparente, pensada para aproximar compradores, vendedores e arrendatários num só espaço digital.
+
+    No Find4U, pode explorar milhares de imóveis, filtrados de acordo com as suas necessidades: localização, preço, tipologia, características especiais e muito mais. Quer esteja à procura da sua nova casa, de um investimento ou de um espaço comercial, reunimos as melhores oportunidades num só lugar.
+
+    Para proprietários e agentes imobiliários, oferecemos uma forma eficiente de divulgar imóveis, alcançar mais interessados e gerir contactos com facilidade. A nossa missão é garantir que cada anúncio é visto pelas pessoas certas — e no momento certo.
+
+    Com o Find4U, encontrar o imóvel ideal nunca foi tão fácil.</p>
+    </div>
+</div>
+
+<div class="row text-center d-flex justify-content-center">
+          <!-- ITEM DA EQUIPA -->
+          <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4">
+              <i class="fa fa-user img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></i>
+              <h5 class="mb-0">Mateus Rodrigues</h5>
+              <span class="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
+          <!-- FIM -->
+
+          <!-- ITEM DA EQUIPA -->
+          <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4">
+              <i class="fa fa-user img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></i>
+              <h5 class="mb-0">Duarte Rastafarai</h5>
+              <span class="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
+          <!-- FIM -->
+
+          <!-- ITEM DA EQUIPA -->
+          <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4">
+              <i class="fa fa-user img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></i>
+              <h5 class="mb-0">Bernardo Teixeira</h5>
+              <span class="small text-uppercase text-muted">CEO - Founder</span>
+            </div>
+          </div>
 
 <section class="faq-section">
         <div class="container-small">
